1e3cd33c797230113c1cd41e99d6837a52f301d9
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2020-07-19 18:29:34 +0100

    GET /user/{id} tests

--- handlers/users_test.go
+++ handlers/users_test.go
@@ -25,7 +25,7 @@ func TestUsersHandler_Get(t *testing.T) {
 		resp := w.Result()
 
 		if resp.StatusCode != http.StatusOK {
-			t.Errorf("expected %d response", http.StatusOK)
+			t.Errorf("expected %d response, got %d", http.StatusOK, resp.StatusCode)
 		}
 
 		if resp.Header.Get("Content-Type") != "application/json" {
@@ -59,7 +59,7 @@ func TestUsersHandler_Get(t *testing.T) {
 		resp := w.Result()
 
 		if resp.StatusCode != http.StatusOK {
-			t.Errorf("expected %d response", http.StatusOK)
+			t.Errorf("expected %d response, got %d", http.StatusOK, resp.StatusCode)
 		}
 
 		if resp.Header.Get("Content-Type") != "application/json" {
@@ -89,6 +89,78 @@ func TestUsersHandler_Get(t *testing.T) {
 		r := httptest.NewRequest("GET", "/users", nil)
 		w := httptest.NewRecorder()
 		uh.Get(w, r)
+
+		resp := w.Result()
+
+		if resp.StatusCode != http.StatusInternalServerError {
+			t.Errorf("expected %d response, got %d", http.StatusInternalServerError, resp.StatusCode)
+		}
+	})
+}
+
+func TestUsersHandler_GetByID(t *testing.T) {
+	t.Run("expect GET /users/{id} to return 200", func(t *testing.T) {
+		mock := newUserRepoMockDefault()
+		mock.findByIDImpl = func(ID string) (*models.User, error) {
+			return mock.mockUsers[0], nil
+		}
+		uh := NewUsersHandler(mock)
+
+		r := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+		uh.GetByID(w, r)
+
+		resp := w.Result()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("expected %d response, got %d", http.StatusOK, resp.StatusCode)
+		}
+
+		if resp.Header.Get("Content-Type") != "application/json" {
+			t.Errorf("expected 'application/json', got '%s'", resp.Header.Get("Content-Type"))
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error("failed to read response body")
+		}
+
+		var user *models.User
+		json.Unmarshal(body, &user)
+
+		if user != mock.mockUsers[0] {
+			t.Error("wrong user returned")
+		}
+	})
+
+	t.Run("expect GET /users/{id} to return 404 when user does not exist", func(t *testing.T) {
+		mock := newUserRepoMockDefault()
+		mock.findByIDImpl = func(ID string) (*models.User, error) {
+			return nil, nil
+		}
+		uh := NewUsersHandler(mock)
+
+		r := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+		uh.GetByID(w, r)
+
+		resp := w.Result()
+
+		if resp.StatusCode != http.StatusNotFound {
+			t.Errorf("expected %d response, got %d", http.StatusNotFound, resp.StatusCode)
+		}
+	})
+
+	t.Run("expect GET /users/{id} to return 500 on internal error", func(t *testing.T) {
+		mock := newUserRepoMockDefault()
+		mock.findByIDImpl = func(ID string) (*models.User, error) {
+			return nil, errors.New("repo error")
+		}
+		uh := NewUsersHandler(mock)
+
+		r := httptest.NewRequest("GET", "/users/1", nil)
+		w := httptest.NewRecorder()
+		uh.GetByID(w, r)
 
 		resp := w.Result()
 
